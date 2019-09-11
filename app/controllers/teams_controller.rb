377e71e76a340c9378ab40057789bc55d12bfbb2
Jake Connors <jake@aeds.com>
2019-09-11 14:24:18 -0500

    Added teams crud actions

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -10,4 +10,32 @@ class TeamsController < ApplicationController
     @team = Team.find(params[:id])
   end
 
+  def create
+    params.require(:team).permit!
+    @team = Team.new(params[:team])
+    if @team.save
+      redirect_to @team, notice: "Successfully created team"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @team = Team.find(params[:id])
+  end
+
+  def update
+    params.require(:team).permit!
+    @team = Team.find(params[:id])
+    if @team.update_attributes(params[:team])
+      redirect_to @team, notice: "Successfully created team"
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @team = Team.new
+  end
+
 end
